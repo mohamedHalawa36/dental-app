5774ce370abc374a728e96352b47d19b56273802
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-04-07 15:24:56 +0200

    fix no result found for home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTodayAppointments } from "~/API/appointments";
+import NoResultsFound from "~/Components/common/NoResultsFound";
 import PatientCard from "~/Components/Patient/PatientCard";
 import PageLayout from "~/Layouts/PageLayout";
 import { formatTime } from "~/lib/utils";
@@ -15,23 +16,23 @@ export default function Home() {
 
   return (
     <PageLayout isFetching={isFetching}>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(19rem,1fr))] max-sm:px-4 p-2 gap-5 ">
-        {appointments?.length && appointments?.length > 0 ? (
-          appointments?.map(({ patient, time, id }) => (
-            <PatientCard
-              key={id}
-              variant={PATIENT_CARD_TYPES.APPOINTMENT}
-              time={formatTime(time)}
-              patient={patient}
-              appointmentId={id}
-            />
-          ))
-        ) : (
-          <p className="text-2xl max-sm:text-base text-center text-slate-500 flex-1">
-            لا توجد مواعيد
-          </p>
-        )}
-      </div>
+      {appointments?.length ? (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(19rem,1fr))] max-sm:py-0 p-2 gap-5 overflow-auto">
+          {appointments?.map(
+            ({ id, patient, time, date }) =>
+              patient && (
+                <PatientCard
+                  key={id}
+                  patient={patient}
+                  variant={PATIENT_CARD_TYPES.APPOINTMENT}
+                  {...{ time: formatTime(time), date, appointmentId: id }}
+                />
+              )
+          )}
+        </div>
+      ) : (
+        <NoResultsFound />
+      )}
     </PageLayout>
   );
 }
