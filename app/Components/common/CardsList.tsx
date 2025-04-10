3e126186e41a03f2068f46d55060727fc80df44b
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-04-10 17:12:02 +0200

    create CardsList component to handle PatientCard rendering

---
+++ app/Components/common/CardsList.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react";
+import { cn } from "~/lib/utils";
+
+export default function CardsList({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-[repeat(auto-fill,minmax(19rem,1fr))] max-sm:py-0 p-2 gap-5 overflow-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
