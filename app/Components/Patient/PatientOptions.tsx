a2832ea68a31fe4cfdb131e108d073c3d9145425
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-04-14 19:51:43 +0200

    implement delete patient

--- app/Components/Patient/PatientOptions.tsx
+++ app/Components/Patient/PatientOptions.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, type ReactNode } from "react";
+import { useEffect, useRef, useState, type ReactNode } from "react";
 import FormModal from "../common/Modals/FormModal";
 import PatientForm from "../Forms/PatientForms/PatientForm";
 import Delete from "../icons/Delete";
@@ -6,21 +6,51 @@ import Details from "../icons/Details";
 import Pencil from "../icons/Pencil";
 import ThreeDots from "../icons/ThreeDots";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import ConfirmModal from "../common/Modals/ConfirmModal";
+import type { PatientApiData } from "~/types/apiData";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deletePatient as deletePatientFunction } from "~/API/patient";
 
-export default function PatientOptions({ patientId }: { patientId: string }) {
+export default function PatientOptions({
+  patient,
+}: {
+  patient: PatientApiData;
+}) {
   const [isOpen, setIsOpen] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
 
-  const optionsRef = useRef<HTMLButtonElement | null>(null);
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  const patientId = patient.id;
+  const patientName = patient.name;
+
+  const queryClient = useQueryClient();
+  const { mutate: deletePatient, isPending: isDeletingPatient } = useMutation<
+    unknown,
+    unknown,
+    string
+  >({
+    mutationFn: deletePatientFunction,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["patients"],
+      });
+    },
+  });
+
+  useEffect(() => {
+    const closeOptions = () => {
+      setIsOpen(false);
+    };
+    document.addEventListener("wheel", closeOptions);
+    return () => document.removeEventListener("wheel", closeOptions);
+  }, [triggerRef.current]);
 
   return (
-    <Popover>
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
       <div className="relative -m-1.5">
-        <PopoverTrigger
-          ref={optionsRef}
-          className=" rounded-full hover:bg-gray-50 transition-all size-8 flex justify-center items-center"
-        >
+        <PopoverTrigger className=" rounded-full hover:bg-gray-50 transition-all size-8 flex justify-center items-center">
           <ThreeDots />
         </PopoverTrigger>
         <PopoverContent
@@ -31,17 +61,33 @@ export default function PatientOptions({ patientId }: { patientId: string }) {
             label="تفاصيل"
             icon={<Details className="size-[22.5px]" />}
           />
-          <button onClick={() => setIsUpdating(true)}>
+          <button ref={triggerRef} onClick={() => setIsUpdating(true)}>
             <OptionBtn
               label="تعديل"
               icon={<Pencil className="-ms-1 h-7 w-8 -me-1" />}
             />
           </button>
 
-          <OptionBtn
-            label="حذف"
-            icon={<Delete className=" fill-red-600 size-[22.5px]" />}
-          />
+          <ConfirmModal
+            isOpen={isDeleting}
+            toggle={() => setIsDeleting((open) => !open)}
+            title="حذف مريض"
+            trigger={
+              <OptionBtn
+                label="حذف"
+                icon={<Delete className=" fill-red-600 size-[22.5px]" />}
+              />
+            }
+            confirmCallBack={() => deletePatient(patientId)}
+            cancelCallBack={() => setIsDeleting(false)}
+            isActionsDisabled={isDeletingPatient}
+          >
+            <p className="font-semibold text-lg">
+              هل تريد حذف ملف &nbsp;
+              <span className=" text-primary">{patientName}</span>
+              &nbsp; ؟
+            </p>
+          </ConfirmModal>
         </PopoverContent>
       </div>
       <FormModal
