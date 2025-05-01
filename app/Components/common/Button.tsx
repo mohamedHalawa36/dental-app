74f4d197bb8e93cb4408f69d561be9fefa228066
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-01 18:25:36 +0300

    create Button component

---
+++ app/Components/common/Button.tsx
@@ -0,0 +1,33 @@
+import { cn } from "~/lib/utils";
+
+type IButton = {
+  label: string;
+  onClick: () => void;
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  className?: string;
+};
+export default function Button({
+  label,
+  disabled,
+  className,
+  variant = "primary",
+}: IButton) {
+  return (
+    <button
+      disabled={disabled}
+      type="submit"
+      className={cn(
+        "p-3  max-sm:w-full w-24 hover:opacity-80 transition font-semibold disabled:bg-gray-400 border border-transparent",
+        {
+          "bg-primary rounded-xl text-muted": variant === "primary",
+          "border-foreground text-foreground bg-transparent":
+            variant === "secondary",
+        },
+        className
+      )}
+    >
+      {label}
+    </button>
+  );
+}
