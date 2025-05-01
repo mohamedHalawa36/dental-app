6d3c15642a1f662aa35023c1f63c1a4c2457359c
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-01 18:27:01 +0300

    create Login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,36 @@
+import { Formik } from "formik";
+import Button from "~/Components/common/Button";
+import InputField from "~/Components/Forms/Fields/InputField";
+
+export default function signup() {
+  return (
+    <Formik initialValues={{}}>
+      <div className=" bg-[linear-gradient(45deg,rgb(var(--secondary)),rgb(var(--primary)))] h-full w-full flex justify-center items-center">
+        <div className="flex flex-col gap-5 w-[25rem] bg-white max-sm:max-w-[90%] max-h-[90%] max-sm:rounded-3xl rounded-[50px] pb-8 pt-3  ">
+          <img
+            src="/images/full-logo.png"
+            alt="logo"
+            className="object-contain w-48 mx-auto max-sm:w-40"
+          />
+          <div className="px-7">
+            <InputField
+              className=" [&>div]:border-primary/50 [&>div]:focus-within:shadow [&>div]:focus-within:border-transparent [&>div]:focus-within:shadow-primary/50 [&>div]:transition"
+              name="name"
+              label="الاسم"
+            />
+            <InputField
+              className=" [&>div]:border-primary/50 [&>div]:focus-within:shadow [&>div]:focus-within:border-transparent [&>div]:focus-within:shadow-primary/50 [&>div]:transition"
+              name="password"
+              label="كلمة السر"
+            />
+            <Button
+              variant="secondary"
+              label="تسجيل الدخول"
+              className="w-full rounded-[1.25rem] max-sm:rounded-xl mt-5 border-primary text-primary hover:bg-primary hover:text-white"
+            />
+          </div>
+        </div>
+      </div>
+    </Formik>
+  );
+}
--- app/routes/signup.tsx
+++
@@ -1,7 +0,0 @@
-export default function signup() {
-  return (
-    <div className=" bg-gradient-to-b to-fuchsia-200/40 from-cyan-200/40 to-70% h-full w-full flex justify-center items-center">
-      <div className="size-48 bg-primary/90 rounded-2xl"></div>
-    </div>
-  );
-}
