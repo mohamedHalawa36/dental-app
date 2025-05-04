160a1b5a688b8c0f3d8575caf9158f7acfa42412
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-04 17:19:05 +0300

    override input border if it has an error

--- app/Components/common/Input.tsx
+++ app/Components/common/Input.tsx
@@ -17,7 +17,7 @@ export default function Input(props: IInputProps) {
         className={cn(
           "flex items-center gap-2 h-10 bg-white rounded-xl px-2 ring-offset-background data-[placeholder]:!text-grey-grey1 text-text-black focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border border-transparent",
           {
-            "border-red-500": !!error,
+            "!border-red-500": !!error,
           }
         )}
       >
