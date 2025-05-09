39be3820b51d7cc4461ba82cdf431db4385c66f7
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-09 18:04:27 +0300

    fix MainFormLayout on wides screens

--- app/Layouts/MainFormLayout.tsx
+++ app/Layouts/MainFormLayout.tsx
@@ -12,16 +12,16 @@ export default function MainFormLayout({
   submitBtn: ReactNode;
 }) {
   return (
-    <Form
-      className={cn(
-        "grid h-full grid-cols-2 gap-8 overflow-auto pe-0.5 focus-within:overflow-hidden max-md:grid-cols-1 max-md:gap-8",
-        className,
-      )}
-    >
-      <div className="mx-1 flex flex-col gap-2">
+    <Form className="flex h-full flex-col gap-2">
+      <div
+        className={cn(
+          "grid h-full grid-cols-2 gap-8 overflow-auto px-1.5 focus-within:overflow-hidden max-md:grid-cols-1 max-md:gap-8",
+          className,
+        )}
+      >
         {children}
-        {submitBtn}
       </div>
+      {submitBtn}
     </Form>
   );
 }
