a5afdb8549961ea6d63037a4dd6622f29026f100
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-09 17:56:38 +0300

    enhance MainFormLayout to disable scrolling on input focus and take a submit button and handle the form from it

--- app/Layouts/MainFormLayout.tsx
+++ app/Layouts/MainFormLayout.tsx
@@ -1,21 +1,27 @@
+import { Form } from "formik";
 import type { ReactNode } from "react";
 import { cn } from "~/lib/utils";
 
 export default function MainFormLayout({
   children,
   className,
+  submitBtn,
 }: {
   children: ReactNode;
   className?: string;
+  submitBtn: ReactNode;
 }) {
   return (
-    <div
+    <Form
       className={cn(
-        "grid grid-cols-2 gap-8 overflow-auto max-md:grid-cols-1 max-md:gap-8",
+        "grid h-full grid-cols-2 gap-8 overflow-auto pe-0.5 focus-within:overflow-hidden max-md:grid-cols-1 max-md:gap-8",
         className,
       )}
     >
-      {children}
-    </div>
+      <div className="mx-1 flex flex-col gap-2">
+        {children}
+        {submitBtn}
+      </div>
+    </Form>
   );
 }
