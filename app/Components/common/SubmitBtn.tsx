0404dc4167a9ff8bdb284b1aa5fa109b438913cb
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-09 17:55:13 +0300

    make submitBtn component take className and remove mt

--- app/Components/common/SubmitBtn.tsx
+++ app/Components/common/SubmitBtn.tsx
@@ -1,15 +1,22 @@
+import { cn } from "~/lib/utils";
+
 export default function SubmitBtn({
   label,
   disabled,
+  className,
 }: {
   label: string;
   disabled: boolean;
+  className?: string;
 }) {
   return (
     <button
       disabled={disabled}
       type="submit"
-      className="mt-3 w-24 rounded-xl bg-primary p-3 font-semibold text-muted transition hover:opacity-80 disabled:bg-gray-400 max-sm:w-full"
+      className={cn(
+        "w-24 rounded-xl bg-primary p-3 font-semibold text-muted transition hover:opacity-80 disabled:bg-gray-400 max-sm:w-full",
+        className,
+      )}
     >
       {label}
     </button>
