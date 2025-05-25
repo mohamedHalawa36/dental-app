faec709422ba1afae960078caf5cea7cebdde75f
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-25 16:19:39 +0300

    expose necessary information from useAuth hook

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,6 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "~/Contexts/AuthContext";
 
-const useAuth = () => useContext(AuthContext);
+const useAuth = () => {
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
+
+  const { role, is_admin, name } = user!;
+
+  const isDoctor = role === "doctor";
+  const isNurse = role === "nurse";
+  const isAdmin = is_admin;
+  const userName = name;
+
+  return { ...authContext, isAdmin, isDoctor, isNurse, userName };
+};
 
 export default useAuth;
