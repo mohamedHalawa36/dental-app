47b45b22b2aa4c6e9fce43a770685f8e01c93836
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-26 14:36:58 +0300

    move isChecking state to auth guard

--- app/Contexts/AuthContext.tsx
+++ app/Contexts/AuthContext.tsx
@@ -1,26 +1,21 @@
-import { createContext, useContext, useState, type ReactNode } from "react";
+import { createContext, useState, type ReactNode } from "react";
 import type { UserProfile } from "~/types/apiData";
 
 type AuthContextArgs = {
   user: UserProfile | null;
   setUser: React.Dispatch<React.SetStateAction<UserProfile | null>>;
-  isChecking: boolean;
-  setIsChecking: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const AuthContext = createContext<AuthContextArgs>({
   user: null,
   setUser: () => {},
-  isChecking: false,
-  setIsChecking: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<UserProfile | null>(null);
-  const [isChecking, setIsChecking] = useState(true);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, isChecking, setIsChecking }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
