2feedfb8494654179942eba04e7d6c3a2dbd8c3f
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2025-05-27 21:34:29 +0300

    add pwa icons and config

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,66 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: "AlYacout",
+        short_name: "Yacout",
+        icons: [
+          {
+            src: "icons/pwa-48x48.webp",
+            sizes: "48x48",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-72x72.webp",
+            sizes: "72x72",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-96x96.webp",
+            sizes: "96x96",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-128x128.webp",
+            sizes: "128x128",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-144x144.webp",
+            sizes: "144x144",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-152x152.webp",
+            sizes: "152x152",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-192x192.webp",
+            sizes: "192x192",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-256x256.webp",
+            sizes: "256x256",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-384x384.webp",
+            sizes: "384x384",
+            type: "image/webp",
+          },
+          {
+            src: "icons/pwa-512x512.webp",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
     }),
   ],
 });
