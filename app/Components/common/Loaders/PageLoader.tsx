aead8a3ff9fa8e3a775ad41529b995a17a91c44c
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-08-29 22:48:05 +0300

    reduce loader size on small screens

--- app/Components/common/Loaders/PageLoader.tsx
+++ app/Components/common/Loaders/PageLoader.tsx
@@ -2,9 +2,9 @@ export default function PageLoader() {
   return (
     <div className="flex h-full items-center justify-center space-x-5">
       <span className="sr-only">Loading...</span>
-      <div className="size-6 animate-bounce rounded-full bg-primary [animation-delay:-0.3s]"></div>
-      <div className="size-6 animate-bounce rounded-full bg-primary [animation-delay:-0.15s]"></div>
-      <div className="size-6 animate-bounce rounded-full bg-primary"></div>
+      <div className="size-6 animate-bounce rounded-full bg-primary [animation-delay:-0.3s] max-sm:size-4"></div>
+      <div className="size-6 animate-bounce rounded-full bg-primary [animation-delay:-0.15s] max-sm:size-4"></div>
+      <div className="size-6 animate-bounce rounded-full bg-primary max-sm:size-4"></div>
     </div>
   );
 }
