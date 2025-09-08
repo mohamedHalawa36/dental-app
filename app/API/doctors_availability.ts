95c1fc34ece02a750740ba9a29d3bea934e82bb2
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-08 18:23:40 +0300

    handle errors for availabilities mutations and get the availabilities sorted by day

--- app/API/doctors_availability.ts
+++ app/API/doctors_availability.ts
@@ -4,45 +4,55 @@ import type {
 } from "~/types/apiData";
 import supabase from "./supabase";
 import { messages } from "./messages";
+import { toast } from "sonner";
 
-const {
-  add: addSuccessMsg,
-  update: updateSuccessMsg,
-  delete: deleteSuccessMsg,
-} = messages.success.appointment;
+const { update: updateSuccessMsg, delete: deleteSuccessMsg } =
+  messages.success.appointment;
+
+const { conflict: conflictMsg } = messages.error.appointment;
 
 export const getDoctorAvailabilty = async (doctorId: string) => {
   return await supabase
     .from("doctor_availability")
     .select()
-    .eq("doctor_id", doctorId);
+    .eq("doctor_id", doctorId)
+    .order("day", { ascending: true });
 };
 
 export const addDoctorAvailabilty = async (
   availabilityData: AddAvailabilityData,
 ) => {
-  return await supabase
+  const { error, status } = await supabase
     .from("doctor_availability")
     .insert([availabilityData])
     .select();
+
+  if (!error) toast.success("تم إضافة الموعد بنجاح");
+  else {
+    if (status === 409) toast.error(conflictMsg);
+  }
 };
 
 export const updateDoctorAvailabilty = async (
   availabilityData: updateAvailabilityData,
 ) => {
-  return await supabase
+  const { error } = await supabase
     .from("doctor_availability")
     .update(availabilityData)
     .eq("id", availabilityData.id)
     .select();
+
+  if (!error) toast.success(updateSuccessMsg);
 };
 
 export const deleteAvailabilty = async (id: number) => {
-  return await supabase
+  const { error } = await supabase
     .from("doctor_availability")
     .delete()
     .eq("id", id)
     .select();
+
+  if (!error) toast.success(deleteSuccessMsg);
 };
 
 export const getOneAvailability = async (
