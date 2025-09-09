54c0db3af650a39d79c5881e7f729941e3867a7a
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-09 15:29:45 +0300

    fix selectField to validate only after the value has changed

--- app/Components/Forms/Fields/SelectField.tsx
+++ app/Components/Forms/Fields/SelectField.tsx
@@ -1,7 +1,8 @@
-import { Field, useFormikContext, type FieldProps } from "formik";
+import { useField, useFormikContext } from "formik";
 import Select, { type SelectProps } from "~/Components/common/Select";
 import type { IFieldProps } from "~/types/components";
 import FieldLayout from "./FieldLayout";
+import { useEffect } from "react";
 
 type SelectFieldProps = Omit<SelectProps, "onValueChange">;
 
@@ -12,33 +13,34 @@ export default function SelectField({
   className,
   ...restProps
 }: SelectFieldProps & IFieldProps) {
-  const { validateOnChange } = useFormikContext();
+  const { validateForm } = useFormikContext();
+  const [field, meta, form] = useField(name);
+
+  const value = field.value;
+
+  useEffect(() => {
+    validateForm();
+  }, [validateForm, value]);
 
   return (
-    <Field name={name}>
-      {({ field, meta, form }: FieldProps) => (
-        <FieldLayout
-          id={id}
-          label={label as string}
-          error={meta.touched ? meta.error : undefined}
-          className={className}
-        >
-          <Select
-            className="w-full rounded-xl border-primary/70 bg-transparent px-2 text-sm"
-            error={
-              meta.touched ? (meta.error ? meta.error : undefined) : undefined
-            }
-            defaultValue={field.value}
-            onValueChange={(value) => {
-              form.setFieldValue(name, value);
-              if (validateOnChange) form.setFieldTouched(name, true);
-            }}
-            {...restProps}
-            name={field.name}
-            // {...field}
-          />
-        </FieldLayout>
-      )}
-    </Field>
+    <FieldLayout
+      id={id}
+      label={label as string}
+      error={meta.touched ? meta.error : undefined}
+      className={className}
+    >
+      <Select
+        className="w-full rounded-xl border-primary/70 bg-transparent px-2 text-sm"
+        error={meta.touched ? (meta.error ? meta.error : undefined) : undefined}
+        defaultValue={field.value}
+        onValueChange={(value) => {
+          form.setValue(value);
+          form.setTouched(true);
+        }}
+        {...restProps}
+        name={field.name}
+        // {...field}
+      />
+    </FieldLayout>
   );
 }
