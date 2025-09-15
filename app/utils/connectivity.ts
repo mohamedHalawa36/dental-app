f38469998b8ff061a0a874ba7db1304ba66d7884
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-16 01:59:01 +0300

    display arabic messages for connectivity toaster and make the offline one not dismissable

--- app/utils/connectivity.ts
+++ app/utils/connectivity.ts
@@ -2,9 +2,14 @@ import { toast } from "sonner";
 
 export const handleConnectionStatus = () => {
   toast.dismiss();
-  if (!navigator.onLine) toast.error("No internet connection");
+  if (!navigator.onLine)
+    toast.error("لايوجد اتصال بالإنترنت", {
+      duration: Infinity,
+      closeButton: false,
+      dismissible: false,
+    });
 
   if (navigator.onLine) {
-    toast.success("Connection Restored");
+    toast.success("تم استعادة الاتصال بالإنترنت");
   }
 };
