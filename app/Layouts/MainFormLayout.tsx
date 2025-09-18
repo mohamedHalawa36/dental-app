1801af9075ab552e336895c50a13f077064087c1
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-18 04:39:54 +0300

    add overlay on all forms to disable it while submitting

--- app/Layouts/MainFormLayout.tsx
+++ app/Layouts/MainFormLayout.tsx
@@ -1,5 +1,6 @@
-import { Form } from "formik";
+import { Form, useFormikContext } from "formik";
 import type { ReactNode } from "react";
+import PageLoader from "~/Components/common/Loaders/PageLoader";
 import { cn } from "~/lib/utils";
 
 export default function MainFormLayout({
@@ -11,14 +12,18 @@ export default function MainFormLayout({
   className?: string;
   submitBtn: ReactNode;
 }) {
+  const { isSubmitting } = useFormikContext();
   return (
     <Form className="flex h-full flex-col gap-2 px-0.5">
       <div
         className={cn(
-          "grid h-full flex-1 grid-cols-2 gap-8 overflow-auto px-1 max-md:grid-cols-1 max-md:gap-2.5",
+          "relative grid h-full flex-1 grid-cols-2 gap-8 overflow-auto px-1 max-md:grid-cols-1 max-md:gap-2.5",
           className,
         )}
       >
+        {isSubmitting && (
+          <PageLoader className="absolute inset-0 m-0 size-full bg-slate-100/70" />
+        )}
         {children}
       </div>
       {submitBtn}
