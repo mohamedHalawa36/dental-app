16b2a198438d73a31ae99b7380cb6f0bc27f6fd8
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-21 12:56:03 +0300

    add necessary functions to change user password

--- app/API/auth.ts
+++ app/API/auth.ts
@@ -1,6 +1,6 @@
 import { toast } from "sonner";
 import type { SignInUserData } from "~/types/apiData";
-import { somethingWentWrongMsg } from "./messages";
+import { somethingWentWrongMsg, somethingWentWrongToastId } from "./messages";
 import supabase, { ApiError } from "./supabase";
 
 export const getUserProfile = async (userId: string) => {
@@ -35,4 +35,35 @@ export const getUserSession = async () => {
   if (!error) return response;
 
   return await supabase.auth.getSession();
+};
+
+export const resetPassword = async (newPassword: string) => {
+  const { data, error } = await supabase.auth.updateUser({
+    password: newPassword,
+  });
+
+  if (!error) {
+    return data;
+  } else {
+    if (error.code === "same_password") {
+      toast.dismiss(somethingWentWrongToastId);
+      toast.error("تم إدخال نفس كلمة السر القديمة، برجاء ادخال كلمة سر جديدة");
+    }
+
+    throw new ApiError(error.message, error.status, error.code);
+  }
+};
+
+export const setResetedPassword = async (
+  userId: string,
+  hasResetedPassword: boolean,
+) => {
+  const res = await supabase
+    .from("profiles")
+    .update({ has_reseted_password: hasResetedPassword })
+    .eq("id", userId)
+    .select()
+    .single();
+
+  return res;
 };
