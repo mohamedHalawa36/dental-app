f1ad5a2a4ce83bd96465cb3ebbbacdb6022f3e11
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-09-25 16:19:31 +0300

    increase login box size

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,7 +3,7 @@ import LoginForm from "~/Components/Forms/LoginForm/LoginForm";
 export default function login() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(45deg,rgb(var(--secondary)),rgb(var(--primary)))]">
-      <div className="flex max-h-[90%] w-[25rem] flex-col gap-5 overflow-hidden rounded-[50px] bg-white pb-8 pt-3 max-sm:max-w-[90%] max-sm:rounded-3xl max-sm:pb-5">
+      <div className="flex max-h-[90%] w-[27rem] flex-col gap-5 overflow-hidden rounded-[50px] bg-white pb-8 pt-3 max-sm:max-w-[90%] max-sm:rounded-3xl max-sm:pb-5">
         <img
           src="/images/full-logo.webp"
           alt="logo"
