2d70271c7f0b05fea7f952c6279c5d9694a57a2f
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-10-04 17:18:57 +0300

    enhance page layout

--- app/routes/_page.tsx
+++ app/routes/_page.tsx
@@ -14,14 +14,14 @@ export default function PageLayout() {
   const hasNew = activeLink?.addNew;
 
   return (
-    <div className="flex h-full w-full flex-col lg:p-4 xl:p-8">
+    <div className="flex h-full w-full flex-col lg:p-4 xl:p-6 2xl:p-7">
       <div className="flex h-full">
         <div className="max-lg:hidden">
           <Sidebar />
         </div>
         <main className="max-h-full flex-1 rounded-e-2xl bg-gradient-to-b from-cyan-200/30 to-fuchsia-200/30 to-70% shadow-xl">
           <PageProvider>
-            <div className="flex h-full w-full flex-col sm:gap-3 sm:px-5 sm:py-6">
+            <div className="flex h-full w-full flex-col sm:gap-3 sm:px-5 sm:pb-3 sm:pt-4">
               <PageHeader title={pageTitle as string} addBtn={hasNew} />
               <div className="flex-1 overflow-auto py-1 pe-1">
                 <Outlet />
