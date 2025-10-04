1aba4d5283fc2d093b1b16e058ebd4da7b731bd3
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-10-04 16:58:17 +0300

    complete work on button component

--- app/Components/common/Button.tsx
+++ app/Components/common/Button.tsx
@@ -1,7 +1,8 @@
+import type { ReactNode } from "react";
 import { cn } from "~/lib/utils";
 
 type IButton = {
-  label: string;
+  children: ReactNode;
   onClick?: () => void;
   variant?: "primary" | "secondary";
   disabled?: boolean;
@@ -9,27 +10,29 @@ type IButton = {
   type?: "submit" | "reset" | "button";
 };
 export default function Button({
-  label,
   disabled,
   className,
   variant = "primary",
   type = "submit",
+  onClick,
+  children,
 }: IButton) {
   return (
     <button
       disabled={disabled}
       type={type}
       className={cn(
-        "border border-transparent p-3 font-semibold transition hover:opacity-80 disabled:bg-gray-400 max-sm:w-full",
+        "rounded-xl border border-transparent p-3 font-semibold transition hover:opacity-80 disabled:cursor-not-allowed disabled:bg-gray-400 max-sm:w-full",
         {
-          "rounded-xl bg-primary text-muted": variant === "primary",
-          "border-foreground bg-transparent text-foreground":
+          "bg-primary text-muted": variant === "primary",
+          "border-foreground bg-transparent text-foreground disabled:bg-black/10":
             variant === "secondary",
         },
         className,
       )}
+      onClick={onClick}
     >
-      {label}
+      {children}
     </button>
   );
 }
