a62ff6a30f413018a060fde664dc0fa71f044209
MohamedHalawa36 <mohammad.halawa@joacademy.com>
2025-10-09 16:05:19 +0300

    pull from main before pull from dev in mergeDev file

--- mergeDev.bash
+++ mergeDev.bash
@@ -2,6 +2,8 @@ git checkout main
 echo "Checked out to main"
 git pull origin dev --no-edit
 echo "Pulled from dev"
+git pull origin main --no-edit
+echo "Pulled from main"
 git push origin main
 echo "Main branch synced"
 git checkout dev
